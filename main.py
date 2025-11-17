8dde1e2cb56c1918cfdf7079cd806a163672e269
SmallWhite <hi.hi.terry123@gmail.com>
2025-11-17 21:44:19 +0800

    add 2 modes

--- main.py
+++ main.py
@@ -87,16 +87,19 @@ def page_difficulty(to_mode):
         nonlocal to_mode
         if to_mode in ("RFH","RFK"):
             to_mode = page_RFHK(to_mode,difficulty)
+        elif to_mode in ("HFR","KFR"):
+            to_mode = page_HKFR(to_mode,difficulty)
         else:
             page_mode()
-    set_screen_size(560, 380)
+    set_screen_size(560, 480)
     clear_screen()
-
     tk.Label(ui, text="=Choose The Difficulty=", font=("Microsoft YaHei",25,"bold"), bg="#dcdde1").pack(fill="x", side="top", pady=5)
     
     tk.Button(ui, text="Easy", font=("Microsoft YaHei",16,"bold"), width=24, height=2, command=lambda:process("e")).pack(side="top", pady=10, padx=20)
     tk.Button(ui, text="Medium", font=("Microsoft YaHei",16,"bold"), width=24, height=2, command=lambda:process("m")).pack(side="top", pady=10, padx=20)
     tk.Button(ui, text="Hard", font=("Microsoft YaHei",16,"bold"), width=24, height=2, command=lambda:process("h")).pack(side="top", pady=10, padx=20)
+    
+    tk.Button(ui, text="=RETURN=", font=("Microsoft YaHei",14,"bold"), width=18, height=2, command=page_mode).pack(side="top", pady=10, padx=20)
 
 def page_mode():
     """
@@ -112,8 +115,8 @@ def page_mode():
     line1.pack()
     
     line2 = tk.Frame(ui, bg="#dcdde1")
-    tk.Button(line2, text="***", font=("Microsoft YaHei",12), width=24, height=2, command=page_mode).pack(side="left", pady=10, padx=20)
-    tk.Button(line2, text="***", font=("Microsoft YaHei",12), width=24, height=2, command=page_mode).pack(side="left", pady=10, padx=20)
+    tk.Button(line2, text="Guess Hiragana from Romaji", font=("Microsoft YaHei",12), width=24, height=2, command=lambda:page_difficulty("HFR")).pack(side="left", pady=10, padx=20)
+    tk.Button(line2, text="Guess Katakana from Romaji", font=("Microsoft YaHei",12), width=24, height=2, command=lambda:page_difficulty("KFR")).pack(side="left", pady=10, padx=20)
     line2.pack()
     
     line3 = tk.Frame(ui, bg="#dcdde1")
@@ -139,13 +142,15 @@ def page_RFHK(mode="RFH",difficulty="e"):
         answer = random.choice(temp_list)
         text_guess.set(answer)
         if difficulty != "h":
-            options = [temp_dict[answer]]
-            while len(options) < len(text_ans):
-                temp = random.choice(temp_list)
-                if temp != answer and temp not in options:
-                    options.append(temp_dict[temp])
+            correct = temp_dict[answer]
+            wrong_choices = []
+            for t in temp_list:
+                if t != answer:
+                    wrong_choices.append(temp_dict[t])
+            count = len(text_ans) - 1
+            options = [correct] + random.sample(wrong_choices, count)
             random.shuffle(options)
-            for i in range(len(text_ans)):
+            for i in range(len(options)):
                 text_ans[i].set(options[i])
         else:
             text_ans[0].set("")
@@ -170,7 +175,6 @@ def page_RFHK(mode="RFH",difficulty="e"):
 
     text_guess = tk.StringVar()
     text_scores = tk.StringVar()
-    
     wrong_times = 0
     correct_times = 0
     set_screen_size(640, 480)
@@ -211,9 +215,92 @@ def page_RFHK(mode="RFH",difficulty="e"):
         tk.Button(options, text="=ENTER=", font=("Microsoft YaHei",18), relief="raised", bd=5, width=8, height=1, command=lambda:check(text_ans[0].get())).pack(side="left", pady=5, padx=5)
         options.pack(side="top")
     
-    btn = tk.Frame(ui, bg="#dcdde1")
-    tk.Button(btn, text="=RETURN=", font=("Microsoft YaHei",20), relief="raised", bd=2, width=10, height=1, command=page_mode).pack(side="left", padx=40)
-    btn.pack(side="top", pady=15)
+    tk.Button(ui, text="=RETURN=", font=("Microsoft YaHei",20), relief="raised", bd=2, width=10, height=1, command=page_mode).pack(side="top", padx=40)
+    ramdom()
+
+def page_HKFR(mode="HFR",difficulty="e"): 
+    """
+    Mode page : Guess Hiragana/Katakana from Romaji
+    """
+    def ramdom():
+        answer = random.choice(temp_list)
+        text_guess.set(answer)
+        correct = temp_dict[answer][temp_index]
+        options = [correct]
+        wrong_choices = []
+        for t in temp_list:
+            if t != answer:
+                wrong_choices.append(temp_dict[t][temp_index])
+        count = len(text_ans) - 1
+        options += random.sample(wrong_choices, count)
+        random.shuffle(options)
+        for i in range(len(options)):
+            text_ans[i].set(options[i])
+    def check(feedback):
+        nonlocal correct_times, wrong_times
+        real_answer = temp_dict[text_guess.get()][temp_index]
+        feedback = feedback.strip()
+        if feedback == real_answer:
+            correct_times += 1
+        else:
+            wrong_times += 1
+        ramdom()
+        text_scores.set(f"✔{correct_times} ✘{wrong_times}")
+
+    temp_list = HKR_list["r"]
+    temp_dict = HKR_dict["r"]
+    temp_index = 0
+    title = "=Guess Hiragana from Romaji="
+    if mode == "KFR":
+        temp_list = HKR_list["r"]
+        temp_dict = HKR_dict["r"]
+        temp_index = 1
+        title = "=Guess Katakana from Romaji="
+
+    text_guess = tk.StringVar()
+    text_scores = tk.StringVar()
+    wrong_times = 0
+    correct_times = 0
+    set_screen_size(640, 480)
+    clear_screen()
+    
+    tk.Label(ui, text=title, font=("Microsoft YaHei",25,"bold"), bg="#dcdde1").pack(fill="x", side="top", pady=5)
+    tk.Label(ui, textvariable=text_guess, font=("Microsoft YaHei",40), relief="solid", bd=5, width=4, height=2).pack(side="top", pady=10)
+    
+    scores = tk.Frame(ui, bg="#dcdde1")
+    tk.Label(scores, textvariable=text_scores, font=("Microsoft YaHei",20),bg="#dcdde1").pack(side="top")
+    scores.pack(side="top")
+    
+    if difficulty in ("e","m","h"):
+        text_ans = [tk.StringVar(),tk.StringVar(),tk.StringVar(),tk.StringVar()]
+        options = tk.Frame(ui, bg="#dcdde1")
+        tk.Button(options, textvariable=text_ans[0], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[0].get())).pack(side="left", pady=5, padx=20)
+        tk.Button(options, textvariable=text_ans[1], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[1].get())).pack(side="left", pady=5, padx=20)
+        tk.Button(options, textvariable=text_ans[2], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[2].get())).pack(side="left", pady=5, padx=20)
+        tk.Button(options, textvariable=text_ans[3], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[3].get())).pack(side="left", pady=5, padx=20)
+        options.pack(side="top")
+    
+    if difficulty in ("m","h"):
+        set_screen_size(640, 580)
+        text_ans += [tk.StringVar(),tk.StringVar(),tk.StringVar(),tk.StringVar()]
+        options = tk.Frame(ui, bg="#dcdde1")
+        tk.Button(options, textvariable=text_ans[4], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[4].get())).pack(side="left", pady=5, padx=20)
+        tk.Button(options, textvariable=text_ans[5], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[5].get())).pack(side="left", pady=5, padx=20)
+        tk.Button(options, textvariable=text_ans[6], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[6].get())).pack(side="left", pady=5, padx=20)
+        tk.Button(options, textvariable=text_ans[7], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[7].get())).pack(side="left", pady=5, padx=20)
+        options.pack(side="top")
+
+    if difficulty == "h":
+        set_screen_size(640, 660)
+        text_ans += [tk.StringVar(),tk.StringVar(),tk.StringVar(),tk.StringVar()]
+        options = tk.Frame(ui, bg="#dcdde1")
+        tk.Button(options, textvariable=text_ans[8], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[8].get())).pack(side="left", pady=5, padx=20)
+        tk.Button(options, textvariable=text_ans[9], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[9].get())).pack(side="left", pady=5, padx=20)
+        tk.Button(options, textvariable=text_ans[10], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[10].get())).pack(side="left", pady=5, padx=20)
+        tk.Button(options, textvariable=text_ans[11], font=("Microsoft YaHei",28), relief="raised", bd=5, width=4, height=1, command=lambda:check(text_ans[11].get())).pack(side="left", pady=5, padx=20)
+        options.pack(side="top")
+
+    tk.Button(text="=RETURN=", font=("Microsoft YaHei",20), relief="raised", bd=2, width=10, height=1, command=page_mode).pack(side="top", pady=10,padx=40)
     ramdom()
 
 def MAIN():
